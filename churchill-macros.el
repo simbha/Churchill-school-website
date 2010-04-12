7d7fc568799a5cd958c4a3908ca802283c1cfba4
JonHurst <jon.a@hursts.eclipse.co.uk>
2010-04-12 20:42:09 +0100

    Added useful macros for those using emacs and nxml-mode

---
+++ churchill-macros.el
@@ -0,0 +1,19 @@
+(fset 'churchill-macro-paragraph
+   [tab ?< ?p ?> ?\C-e ?< ?/ ?\M-q ?\C-f])
+(global-set-key [?\C-x ?\C-k ?p] 'churchill-macro-paragraph)
+
+(fset 'churchill-macro-h2
+   [?\C-a tab ?< ?h ?2 ?> ?\C-e ?< ?/ ?\M-q ?\C-f])
+(global-set-key [?\C-x ?\C-k ?2] 'churchill-macro-h2)
+
+(fset 'churchill-macro-h3
+   [?\C-a tab ?< ?h ?3 ?> ?\C-e ?< ?/ ?\M-q ?\C-f])
+(global-set-key [?\C-x ?\C-k ?3] 'churchill-macro-h3)
+
+(fset 'churchill-macro-tablerow
+   [?\C-a ?\M-\\ tab ?< ?t ?r ?> ?< ?t ?h ?> ?\M-c ?\C-f ?\C-f ?< ?/ ?< ?t ?d ?> ?\M-\\ ?\C-e ?< ?/ ?< ?/ ?\C-f])
+(global-set-key [?\C-x ?\C-k ?r] 'churchill-macro-tablerow)
+
+(fset 'churchill-macro-nbsp
+   "\C-d&nbsp;")
+(global-set-key [?\C-x ?\C-k ?n] 'churchill-macro-nbsp)
