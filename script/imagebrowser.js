58da9dad33d33eaff08654e9904a8e00efa1a62a
JonHurst <jon.a@hursts.eclipse.co.uk>
2010-06-30 15:13:26 +0100

    Moved suckerfish code from imagebrowser.js to suckerfish.js
    suckerfish.js is now only run by IE6 (it is a fix up for the poor CSS
    implementation in this browser), being triggered by a conditional comment.

--- script/imagebrowser.js
+++ script/imagebrowser.js
@@ -49,18 +49,4 @@ function last_photo()
     return false;
 }
     
-
-sfHover = function() {
-    var sfEls = document.getElementById("buttonbar").getElementsByTagName("LI");
-    for (var i=0; i<sfEls.length; i++) {
-	sfEls[i].onmouseover=function() {
-	    this.className+=" sfhover";
-	}
-	sfEls[i].onmouseout=function() {
-	    this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-	}
-    }
-}
-
-if (window.attachEvent) window.attachEvent("onload", sfHover);
 var current_photo = 0
--- script/suckerfish.js
+++ script/suckerfish.js
@@ -1,13 +1,28 @@
+//This script should only be run by IE6
+//Its purpose is to fix up the dodgy CSS implementation of hover in this browser
+
 sfHover = function() {
-	var sfEls = document.getElementById("navigation").getElementsByTagName("LI");
-	for (var i=0; i<sfEls.length; i++) {
-		sfEls[i].onmouseover=function() {
-			this.className+=" sfhover";
-		}
-		sfEls[i].onmouseout=function() {
-			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-		}
+    var sfEls = document.getElementById("navigation").getElementsByTagName("LI");
+    for (var i=0; i<sfEls.length; i++) {
+	sfEls[i].onmouseover=function() {
+	    this.className+=" sfhover";
 	}
+	sfEls[i].onmouseout=function() {
+	    this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+	}
+    }
+    if(document.getElementById("buttonbar"))
+    {
+        sfEls=document.getElementById("buttonbar").getElementsByTagName("LI");
+        for (var i=0; i<sfEls.length; i++) {
+	    sfEls[i].onmouseover=function() {
+	        this.className+=" sfhover";
+	    }
+	    sfEls[i].onmouseout=function() {
+	        this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+	    }
+        }
+    }
 }
 
 window.attachEvent("onload", sfHover);
