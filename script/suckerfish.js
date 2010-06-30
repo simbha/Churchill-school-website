cf8026811827d20cf8e6c9a1f3f6824100aee08d
JonHurst <jon.a@hursts.eclipse.co.uk>
2010-06-30 16:06:27 +0100

    Improved new suckerfish function

--- script/suckerfish.js
+++ script/suckerfish.js
@@ -2,26 +2,26 @@
 //Its purpose is to fix up the dodgy CSS implementation of hover in this browser
 
 sfHover = function() {
+    //Unfortunately, its not possible to join two nodeLists - we therefore have to
+    //push everything into an array, which is a tad messy. Ho hum.
+    var sfElArray = []
     var sfEls = document.getElementById("navigation").getElementsByTagName("LI");
-    for (var i=0; i<sfEls.length; i++) {
-	sfEls[i].onmouseover=function() {
-	    this.className+=" sfhover";
-	}
-	sfEls[i].onmouseout=function() {
-	    this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-	}
-    }
+    for (var i=0; i<sfEls.length; i++)
+        sfElArray.push(sfEls[i])
     if(document.getElementById("buttonbar"))
     {
         sfEls=document.getElementById("buttonbar").getElementsByTagName("LI");
-        for (var i=0; i<sfEls.length; i++) {
-	    sfEls[i].onmouseover=function() {
-	        this.className+=" sfhover";
-	    }
-	    sfEls[i].onmouseout=function() {
-	        this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-	    }
-        }
+        for (var i=0; i<sfEls.length; i++)
+            sfElArray.push(sfEls[i])
+    }
+    for (var i=0; i<sfElArray.length; i++)
+    {
+	sfElArray[i].onmouseover=function() {
+	    this.className+=" sfhover";
+	}
+	sfElArray[i].onmouseout=function() {
+	    this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+	}
     }
 }
 
